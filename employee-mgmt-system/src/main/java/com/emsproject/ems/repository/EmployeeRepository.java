bfc360e48dc728b135405394f8226aba22619a87
Dilip Chaudhary <dilipcd1997@gmail.com>
2021-11-16 23:45:06 +0530

    Employee Management System Project

---
+++ employee-mgmt-system/src/main/java/com/emsproject/ems/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.emsproject.ems.repository;
+
+import com.emsproject.ems.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
